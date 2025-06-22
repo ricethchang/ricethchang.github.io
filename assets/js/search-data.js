dbc561f5292ce19f517ef22699b8a583a06a90da
ricethchang <taudch@gmail.com>
2025-06-22 13:43:44 +0000

    Deploying to gh-pages from @ ricethchang/ricethchang.github.io@ad3a7e19c42140a48a0d0d13ced5fc2488ee5800 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -132,28 +132,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/geojson-map/";
           
         },
-      },{id: "post-a-post-with-echarts",
-        
-          title: "a post with echarts",
-        
-        description: "this is what included echarts code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/echarts/";
-          
-        },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/chartjs/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
